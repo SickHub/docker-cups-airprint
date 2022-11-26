27bda1e6facaf56160ad72997755770e99ef3240
stefan-golinschi <stefan.golinschI@gmail.com>
2022-11-26 14:50:42 +0300

    fix: escape special char when using grep (#57)

--- start-cups.sh
+++ start-cups.sh
@@ -38,7 +38,7 @@ fi
 sed -i 's/^.*AccessLog .*/AccessLog stderr/' /etc/cups/cups-files.conf
 sed -i 's/^.*ErrorLog .*/ErrorLog stderr/' /etc/cups/cups-files.conf
 sed -i 's/^.*PageLog .*/PageLog stderr/' /etc/cups/cups-files.conf
-if [ "yes" = "${CUPS_REMOTE_ADMIN}" -a -z "$(grep "^Listen *:631" /etc/cups/cupsd.conf)" ]; then
+if [ "yes" = "${CUPS_REMOTE_ADMIN}" -a -z "$(grep "^Listen \*:631" /etc/cups/cupsd.conf)" ]; then
   [ -z "$(grep "^Listen localhost:631" /etc/cups/cupsd.conf)" ] &&
     echo "Listen *:631" >> /etc/cups/cupsd.conf ||
     sed -i 's/Listen localhost:631/Listen *:631/' /etc/cups/cupsd.conf
