8ccbcdaaddc578492d9336d4ec841aec9dc210bf
teoli <122423749+theyo-tester@users.noreply.github.com>
2025-01-17 09:59:35 +0100

    Update pre-init-script.sh
    Simplify script. No need for variable here. Just go with the default value in the info

--- pre-init-script.sh
+++ pre-init-script.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-PRE_INIT_HOOK=${PRE_INIT_HOOK:="/root/pre-init-script.sh"}
 
 echo "This is an example/dummy pre-init script. It does nothing by default.
 If you want to execute your own code & logic (e.g., installing a printer driver via a shell script or apt)
 before everything initializes and starts up 
 then:
-    a) Replace this file via a volume mount (-v /path/to/host/script:${PRE_INIT_HOOK})
+    a) Replace this file via a volume mount (-v /path/to/host/script:/root/pre-init-script.sh)
 or  
-    b) Change the environment variable 'PRE_INIT_HOOK' to your custom path or command 
+    b) Change the environment variable 'PRE_INIT_HOOK' to your custom (mounted) path or command 
        that should be executed inside the container."
